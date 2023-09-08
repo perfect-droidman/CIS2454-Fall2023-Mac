e36dc662af8abe8cdfea91ac48b153e14c2f688a
Gabriele Grosse <ggross7577@gmail.com>
2023-09-08 11:46:50 -0400

    Update index.php

--- Lab0/index.php
+++ Lab0/index.php
@@ -8,6 +8,7 @@
     <body>
         <?php
         echo "<h2>Tahreem Bhatti</h2>";
+        
         echo "Tahreem Bhatti I am doing computer software engineering I am taking this class because it's required for my degree.";
         echo "I took all the cis class for this degree and it's my last semester in occ so all the class that counts for the degree.";
         echo "I love to go hiking and dancing too.";
